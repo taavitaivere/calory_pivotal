464de6b46f7b452bdf136e279b8b00a788b8131c
Taavi taivere <taavi.taivere@khk.ee>
2022-01-20 13:32:34 +0200

    As a user i can see meal names and their calories [Delivers #180975908]

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-const StorageCtrl = (function () {
-    return {
-        storeItem: function (item) {
-            let items;
-
-            if(localStorage.getItem("items") === null){
-                items = [];
-                items.push(item);
-                localStorage.setItem("items", JSON.stringify(items));
-            } else {
-                items = JSON.parse(localStorage.getItem("items"));
-
-                items.push(item);
-
-                localStorage.setItem("items", JSON.stringify(items));
-            }
-        },
-        getItemsFromStorage: function () {
-            let items;
-            if(localStorage.getItem("items") === null){
-                items = [];
-            } else {
-                items = JSON.parse(localStorage.getItem("items"));
-            }
-            return items;
-        }
-    }
-})();
 const ItemCtrl = (function () {
 
     const Item = function (id, name, calories){
@@ -46,27 +18,6 @@ const ItemCtrl = (function () {
         getItems: function () {
             return data.items
         },
-        addItem: function (name, calories) {
-            let ID;
-            if(data.items.length > 0){
-                ID = data.items[data.items.length - 1].id + 1
-            } else {
-                ID = 0
-            }
-            calories = parseInt(calories);
-            newItem = new Item(ID, name, calories);
-            data.items.push(newItem);
-            return newItem
-        },
-        getTotalCalories: function () {
-            let total = 0;
-
-            data.items.forEach(function (item) {
-                total = total + item.calories;
-            });
-            data.total = total;
-            return data.total;
-        },
         logData: function (){
             return data
         }
@@ -74,13 +25,6 @@ const ItemCtrl = (function () {
 })();
 
 const UICtrl = (function () {
-    const UISelectors = {
-        itemList: "#item-list",
-        itemNameInput: "#item-name",
-        itemCaloriesInput: "#item-calories",
-        addBtn:".add-btn",
-        totalCalories: ".total-calories"
-    }
     return {
         populateItemList: function (items) {
             let html = " ";
@@ -88,78 +32,24 @@ const UICtrl = (function () {
             items.forEach(function (item) {
                 html += `<li class="collection-item" id="item-${item.id}">
                 <strong>${item.name}:</strong><em>${item.calories} Calories</em>
-                <a href="#" class="secondary-content">
+                <a herf="#" class="secondary-content">
                 <i class="edit-item fa fa-pencil"></i>
                 </a>
                 </li>`;
             });
-            document.querySelector(UISelectors.itemList).innerHTML=html;
-        },
-        getSelectors: function () {
-            return UISelectors
-        },
-        getItemInput: function () {
-            return {
-                name: document.querySelector(UISelectors.itemNameInput).value,
-                calories: document.querySelector(UISelectors.itemCaloriesInput).value
-            }
-        },
-        addListItem: function (item) {
-            const li = document.createElement("li");
-
-            li.className = "collection-item";
-
-            li.id = `item-${item.id}`;
-            li.innerHTML = `<strong>${item.name}: </strong><en>${item.calories} Calories</en>
-             <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i>
-               </a>`;
-            document.querySelector(UISelectors.itemList).insertAdjacentElement("beforeend",li)
-        },
-        clearInput: function () {
-            document.querySelector(UISelectors.itemNameInput).value = " ";
-            document.querySelector(UISelectors.itemNameInput).value = " ";
-        },
-        showTotalCalories: function (totalCalories) {
-            document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+            document.querySelector("#item-list").innerHTML=html;
         }
     }
 })();
-
-
-
-const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
-    const loadEventListeners = function () {
-        const UISelectors = UICtrl.getSelectors();
-        document.querySelector(UISelectors.addBtn).addEventListener("click", itemAddSubmit);
-        document.addEventListener("DOMContentLoaded", getItemsFromStorage)
-    }
-    const itemAddSubmit = function (event) {
-        const input = UICtrl.getItemInput()
-        if(input.name !== " " && input.calories !== " "){
-            const newItem = ItemCtrl.addItem(input.name, input.calories)
-            UICtrl.addListItem(newItem)
-            const totalCalories = ItemCtrl.getTotalCalories();
-            UICtrl.showTotalCalories(totalCalories);
-            StorageCtrl.storeItem(newItem);
-            UICtrl.clearInput();
-        }
-        event.preventDefault()
-    }
-    const getItemsFromStorage = function () {
-
-        const items = StorageCtrl.getItemsFromStorage()
-
-        UICtrl.populateItemList(items)
-
-    }
+const App = (function (ItemCtrl, UiCtrl) {
     return{
         init: function () {
             console.log("Initializing App")
             const items = ItemCtrl.getItems()
             UICtrl.populateItemList(items)
-            loadEventListeners();
+            console.log(items)
         }
     }
-})(ItemCtrl, StorageCtrl, UICtrl);
+})(ItemCtrl,UICtrl);
 
 App.init()
